7567b69fdcc181d21313c142d772c733abc65311
owuordickson <owuordickson@gmail.com>
2019-12-10 15:54:07 +0300

    added

--- hpc@lr/crossing.sh
+++ hpc@lr/crossing.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=crossing
 #SBATCH --output=res_x.txt
 #SBATCH -n 28
-#SBATCH --time=1:00:00
+#SBATCH --time=03:00:00
 #SBATCH --partition=lirmm
 #SBATCH --account=pgpm
 #SBATCH --mail-user=dickson-odhiambo.owuor@lirmm.fr
 
 module load python/3.7.2
-python3 crossing/src/init_fuzztx_csv.py -a 0 -f 'data/GPS_site8.csv,data/Omnidir_site8.csv' -c 28
+python3 crossing/src/init_fuzztx_csv.py -a 0 -f 'data/Omnidir_site80.csv,data/Directio_site80.csv' -c 28
