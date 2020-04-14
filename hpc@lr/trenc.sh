db610682f5ce600879a1869b4f5102fb9b207c5f
owuordickson <owuordickson@gmail.com>
2020-04-14 22:30:42 +0300

    added

--- hpc@lr/trenc.sh
+++ hpc@lr/trenc.sh
@@ -9,4 +9,4 @@
 #SBATCH --mail-user=dickson-odhiambo.owuor@lirmm.fr
 
 module load python/3.7.2
-python3 trenc/init_trenc.py -f data/Omnidir_site81.csv -m 28
+python3 trenc/init_trenc.py -f data/Omnidir_site2k.csv -m 28
