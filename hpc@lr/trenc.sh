26df8d0aa286ab50145232ea011b7f46b8fbf9cb
owuordickson <owuordickson@gmail.com>
2020-05-13 14:22:44 +0300

    added

--- hpc@lr/trenc.sh
+++ hpc@lr/trenc.sh
@@ -2,11 +2,11 @@
 #
 #SBATCH --job-name=trenc
 #SBATCH --output=res_trenc.txt
-#SBATCH -n 28
+#SBATCH -n 5
 #SBATCH --time=72:00:00
 #SBATCH --partition=lirmm
 #SBATCH --account=pgpm
 #SBATCH --mail-user=dickson-odhiambo.owuor@lirmm.fr
 
 module load python/3.7.2
-python3 trenc/init_trenc.py -f data/ICU_household_power_consumption15k.csv -m 28
+python3 trenc/init_trenc.py -f data/ICU_household_power_consumption5k.csv -m 5
