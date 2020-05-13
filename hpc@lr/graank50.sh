4836332f0ca5892fe30cbac0503024b7271882a3
owuordickson <owuordickson@gmail.com>
2020-05-13 09:30:05 +0300

    added

--- hpc@lr/graank50.sh
+++ hpc@lr/graank50.sh
@@ -2,11 +2,11 @@
 #
 #SBATCH --job-name=graank
 #SBATCH --output=res_graank.txt
-#SBATCH -n 28
+#SBATCH -n 10
 #SBATCH --time=72:00:00
 #SBATCH --partition=lirmm
 #SBATCH --account=pgpm
 #SBATCH --mail-user=dickson-odhiambo.owuor@lirmm.fr
 
 module load python/3.7.2
-python3 ant/src/init_graank.py -f data/ICU_household_power_consumption50k.csv -c 28
+python3 ant/src/init_graank.py -f data/ICU_household_power_consumption50k.csv -c 10
