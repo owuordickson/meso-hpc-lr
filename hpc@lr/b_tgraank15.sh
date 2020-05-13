f27e65e2b23de9bcd2bf0994d8746df1b9a979fc
owuordickson <owuordickson@gmail.com>
2020-05-13 15:47:20 +0300

    added

---
+++ hpc@lr/b_tgraank15.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH --job-name=border
+#SBATCH --output=res_border.txt
+#SBATCH -n 15
+#SBATCH --time=72:00:00
+#SBATCH --partition=lirmm
+#SBATCH --account=pgpm
+#SBATCH --mail-user=dickson-odhiambo.owuor@lirmm.fr
+
+module load python/3.7.2
+python3 trenc/init_bordertgraank.py -f data/ICU_household_power_consumption5k.csv -m 15
---
+++ hpc@lr/trenc2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#SBATCH --job-name=trenc
+#SBATCH --output=res_trenc.txt
+#SBATCH -n 2
+#SBATCH --time=72:00:00
+#SBATCH --partition=lirmm
+#SBATCH --account=pgpm
+#SBATCH --mail-user=dickson-odhiambo.owuor@lirmm.fr
+
+module load python/3.7.2
+python3 trenc/init_trenc.py -f data/ICU_household_power_consumption5k.csv -m 2
