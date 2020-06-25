9af6012a943a729e0f16aae4d5d78c86d49b9f90
owuordickson <owuordickson@gmail.com>
2020-06-25 23:46:30 +0300

    added

--- hpc@lr/aco_grad.sh
+++ hpc@lr/aco_grad.sh
@@ -10,3 +10,25 @@
 
 module load python/3.8.2
 python3 ant/src/init_acograd.py -f data/Directio_site32k.csv -c 112 -s 0.5
+python3 ant/src/init_acograd.py -f data/Directio_site32k.csv -c 112 -s 0.6
+python3 ant/src/init_acograd.py -f data/Directio_site32k.csv -c 112 -s 0.7
+python3 ant/src/init_acograd.py -f data/Directio_site32k.csv -c 112 -s 0.8
+python3 ant/src/init_acograd.py -f data/Directio_site32k.csv -c 112 -s 0.9
+
+python3 ant/src/init_acograd.py -f data/UCI_household_power_consumption116k.csv -c 112 -s 0.5
+python3 ant/src/init_acograd.py -f data/UCI_household_power_consumption116k.csv -c 112 -s 0.6
+python3 ant/src/init_acograd.py -f data/UCI_household_power_consumption116k.csv -c 112 -s 0.7
+python3 ant/src/init_acograd.py -f data/UCI_household_power_consumption116k.csv -c 112 -s 0.8
+python3 ant/src/init_acograd.py -f data/UCI_household_power_consumption116k.csv -c 112 -s 0.9
+
+python3 ant/src/init_acograd.py -f data/c2k.csv -c 112 -s 0.5
+python3 ant/src/init_acograd.py -f data/c2k.csv -c 112 -s 0.6
+python3 ant/src/init_acograd.py -f data/c2k.csv -c 112 -s 0.7
+python3 ant/src/init_acograd.py -f data/c2k.csv -c 112 -s 0.8
+python3 ant/src/init_acograd.py -f data/c2k.csv -c 112 -s 0.9
+
+python3 ant/src/init_acograd.py -f data/breast_cancer.csv -c 112 -s 0.5
+python3 ant/src/init_acograd.py -f data/breast_cancer.csv -c 112 -s 0.6
+python3 ant/src/init_acograd.py -f data/breast_cancer.csv -c 112 -s 0.7
+python3 ant/src/init_acograd.py -f data/breast_cancer.csv -c 112 -s 0.8
+python3 ant/src/init_acograd.py -f data/breast_cancer.csv -c 112 -s 0.9
--- hpc@lr/graank.sh
+++ hpc@lr/graank.sh
@@ -14,3 +14,21 @@ python3 ant/src/init_graank.py -f data/Directio_site32k.csv -c 112 -s 0.6
 python3 ant/src/init_graank.py -f data/Directio_site32k.csv -c 112 -s 0.7
 python3 ant/src/init_graank.py -f data/Directio_site32k.csv -c 112 -s 0.8
 python3 ant/src/init_graank.py -f data/Directio_site32k.csv -c 112 -s 0.9
+
+python3 ant/src/init_graank.py -f data/ICU_household_power_consumption116k.csv -c 112 -s 0.5
+python3 ant/src/init_graank.py -f data/ICU_household_power_consumption116k.csv -c 112 -s 0.6
+python3 ant/src/init_graank.py -f data/ICU_household_power_consumption116k.csv -c 112 -s 0.7
+python3 ant/src/init_graank.py -f data/ICU_household_power_consumption116k.csv -c 112 -s 0.8
+python3 ant/src/init_graank.py -f data/ICU_household_power_consumption116k.csv -c 112 -s 0.9
+
+python3 ant/src/init_graank.py -f data/c2k.csv -c 112 -s 0.5
+python3 ant/src/init_graank.py -f data/c2k.csv -c 112 -s 0.6
+python3 ant/src/init_graank.py -f data/c2k.csv -c 112 -s 0.7
+python3 ant/src/init_graank.py -f data/c2k.csv -c 112 -s 0.8
+python3 ant/src/init_graank.py -f data/c2k.csv -c 112 -s 0.9
+
+python3 ant/src/init_graank.py -f data/breast_cancer.csv -c 112 -s 0.5
+python3 ant/src/init_graank.py -f data/breast_cancer.csv -c 112 -s 0.6
+python3 ant/src/init_graank.py -f data/breast_cancer.csv -c 112 -s 0.7
+python3 ant/src/init_graank.py -f data/breast_cancer.csv -c 112 -s 0.8
+python3 ant/src/init_graank.py -f data/breast_cancer.csv -c 112 -s 0.9
