7012c523b84a7cfc91019d624d1f683affc4454f
owuordickson <owuordickson@gmail.com>
2020-07-20 18:31:11 +0300

    added

--- hpc@lr/aco_grad.sh
+++ hpc@lr/aco_grad.sh
@@ -9,10 +9,6 @@
 #SBATCH --mail-user=dickson-odhiambo.owuor@lirmm.fr
 
 module load python/3.7.2
-python3 ant/src/init_acograd.py -f data/c2k.csv -c 14 -s 0.9
-python3 ant/src/init_acograd.py -f data/Directio_site15k.csv -c 14 -s 0.9
-python3 ant/src/init_acograd.py -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.9
-
-python3 ant/src/init_acograd.py -f data/c2k_02k.csv -c 14 -s 0.9
-python3 ant/src/init_acograd.py -f data/Directio_site02k.csv -c 14 -s 0.9
-python3 ant/src/init_acograd.py -f data/UCI_household_power_consumption1k.csv -c 14 -s 0.9
+python3 ant/src/init_acograd.py -f data/c2k.csv -c 14 -s 0.6
+python3 ant/src/init_acograd.py -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.7
+python3 ant/src/init_acograd.py -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.8
