35cb5282ba463dd80c02b2aec7854085bb0cda60
owuordickson <owuordickson@gmail.com>
2020-07-20 12:52:41 +0300

    added

--- hpc@lr/aco_grad.sh
+++ hpc@lr/aco_grad.sh
@@ -13,6 +13,6 @@ python3 ant/src/init_acograd.py -f data/c2k.csv -c 14 -s 0.9
 python3 ant/src/init_acograd.py -f data/Directio_site15k.csv -c 14 -s 0.9
 python3 ant/src/init_acograd.py -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.9
 
-python3 ant/src/init_acograd.py -f data/c2k_06k.csv -c 14 -s 0.9
+python3 ant/src/init_acograd.py -f data/c2k_02k.csv -c 14 -s 0.9
 python3 ant/src/init_acograd.py -f data/Directio_site02k.csv -c 14 -s 0.9
 python3 ant/src/init_acograd.py -f data/UCI_household_power_consumption1k.csv -c 14 -s 0.9
