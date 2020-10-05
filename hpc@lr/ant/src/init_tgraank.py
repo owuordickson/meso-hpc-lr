7ff9717f3ee263afd5cb40b897e9524b6951e948
owuordickson <owuordickson@gmail.com>
2020-10-05 18:20:19 +0200

    added

--- hpc@lr/ant/src/init_tgraank.py
+++ hpc@lr/ant/src/init_tgraank.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
         optparser.add_option('-c', '--refColumn',
                              dest='refCol',
                              help='reference column',
-                             default=1,
+                             default=2,
                              type='int')
         optparser.add_option('-s', '--minSupport',
                              dest='minSup',
