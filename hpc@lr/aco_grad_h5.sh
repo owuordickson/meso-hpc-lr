cf00dfc4d42c328a086d7bf06ff4187bcd38090a
owuordickson <owuordickson@gmail.com>
2021-02-17 17:50:57 +0300

    added aco_h5

---
+++ hpc@lr/aco_grad_h5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=aco-grad
+#SBATCH --output=res_acograd.txt
+#SBATCH -n 14
+#SBATCH --time=72:00:00
+#SBATCH --partition=lirmm
+#SBATCH --account=pgpm
+#SBATCH --mail-user=dickson-odhiambo.owuor@lirmm.fr
+
+module load python/3.7.2
+python3 ant_h5/src/init_acograd_h5.py -f data/c2k.csv -c 14 -s 0.6
+python3 ant_h5/src/init_acograd_h5.py -f data/UCI_household_power_consumption116k.csv -c 14 -s 0.6
