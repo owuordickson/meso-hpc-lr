28325190facdb79962c8fa2d444e5831be36ad37
owuordickson <owuordickson@gmail.com>
2021-02-25 10:03:11 +0300

    added

--- hpc@lr/ant_h5/src/init_acograd_v4.py
+++ hpc@lr/ant_h5/src/init_acograd_v4.py
@@ -21,7 +21,7 @@ Description:
 import sys
 from optparse import OptionParser
 # from common.profile_mem import Profile
-from algorithms.ant_colony import GradACO
+from algorithms.ant_colony.aco_grad_v4 import GradACO
 
 
 def init_algorithm(f_path, min_supp, cores):
