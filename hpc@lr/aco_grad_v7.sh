154c43caf9c884567eae47c7a6f1e2ed4f730010
owuordickson <owuordickson@gmail.com>
2021-03-05 10:34:00 +0300

    updated

--- hpc@lr/aco_grad_v7.sh
+++ hpc@lr/aco_grad_v7.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --job-name=aco-v7
 #SBATCH --output=res_acogradv7.txt
-#SBATCH -n 28
+#SBATCH -n 14
 #SBATCH --time=72:00:00
 #SBATCH --partition=lirmm
 #SBATCH --account=pgpm
@@ -15,4 +15,4 @@ module load python/3.8.2
 
 # python3 ant_h5/src/init_acograd_v5.py -f data/c2k.csv -c 14 -s 0.6
 # python3 ant_h5/src/init_acograd_v5.py -f data/UCI_household_power_consumption116k.csv -c 14 -s 0.6
-python3 ant_h5/src/init_acograd_v7.py -f data/UCI_household_power_consumption500k.csv -c 28 -s 0.9
+python3 ant_h5/src/init_acograd_v7.py -f data/UCI_household_power_consumption500k.csv -c 14 -s 0.9
