767ed1ccc34cdc62300d795cbddb1fd04f69d87e
owuordickson <owuordickson@gmail.com>
2021-04-30 22:46:39 +0300

    updated

--- hpc@lr/aco_grad.sh
+++ hpc@lr/aco_grad.sh
@@ -9,6 +9,17 @@
 #SBATCH --mail-user=dickson-odhiambo.owuor@lirmm.fr
 
 module load python/3.7.2
-python3 ant/src/init_acograd.py -f data/c2k.csv -c 14 -s 0.6
-python3 ant/src/init_acograd.py -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.7
-python3 ant/src/init_acograd.py -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.8
+python3 ant/src/algorithms/aco/init_acograd.py -f data/breast_cancer.csv -c 14 -s 0.5
+python3 ant/src/algorithms/aco/init_acograd.py -f data/c2k.csv -c 14 -s 0.5
+python3 ant/src/algorithms/aco/init_acograd.py -f data/Directio_site15k.csv -c 14 -s 0.5
+python3 ant/src/algorithms/aco/init_acograd.py -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
+
+python3 ant/src/algorithms/ga/init_gagrad.py -f data/breast_cancer.csv -c 14 -s 0.5
+python3 ant/src/algorithms/ga/init_gagrad.py -f data/c2k.csv -c 14 -s 0.5
+python3 ant/src/algorithms/ga/init_gagrad.py -f data/Directio_site15k.csv -c 14 -s 0.5
+python3 ant/src/algorithms/ga/init_gagrad.py -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
+
+python3 ant/src/algorithms/pso/init_psograd.py -f data/breast_cancer.csv -c 14 -s 0.5
+python3 ant/src/algorithms/pso/init_psograd.py -f data/c2k.csv -c 14 -s 0.5
+python3 ant/src/algorithms/pso/init_psograd.py -f data/Directio_site15k.csv -c 14 -s 0.5
+python3 ant/src/algorithms/pso/init_psograd.py -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
