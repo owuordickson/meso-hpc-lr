076c7beb48195335f2f5515ad7049f49454280d3
owuordickson <owuordickson@gmail.com>
2021-05-06 09:35:40 +0300

    added

--- hpc@lr/aco_grad.sh
+++ hpc@lr/aco_grad.sh
@@ -11,49 +11,49 @@
 module load python/3.8.2
 python3 ant/src/pkg_main/main.py -a 'aco' -f data/breast_cancer.csv -c 28 -s 0.5
 python3 ant/src/pkg_main/main.py -a 'aco' -f data/c2k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/Directio_site15k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/UCI_household_power_consumption50k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/Directio_site6k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/UCI_household_power_consumption19k.csv -c 28 -s 0.5
 
 python3 ant/src/pkg_main/main.py -a 'ga' -f data/breast_cancer.csv -c 28 -s 0.5
 python3 ant/src/pkg_main/main.py -a 'ga' -f data/c2k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/Directio_site15k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/UCI_household_power_consumption50k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/Directio_site6k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/UCI_household_power_consumption19k.csv -c 28 -s 0.5
 
 python3 ant/src/pkg_main/main.py -a 'pso' -f data/breast_cancer.csv -c 28 -s 0.5
 python3 ant/src/pkg_main/main.py -a 'pso' -f data/c2k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/Directio_site15k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/UCI_household_power_consumption50k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/Directio_site6k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/UCI_household_power_consumption19k.csv -c 28 -s 0.5
 
 
 
 python3 ant/src/pkg_main/main.py -a 'aco' -f data/breast_cancer.csv -c 28 -s 0.5
 python3 ant/src/pkg_main/main.py -a 'aco' -f data/c2k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/Directio_site15k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/UCI_household_power_consumption50k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/Directio_site6k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/UCI_household_power_consumption19k.csv -c 28 -s 0.5
 
 python3 ant/src/pkg_main/main.py -a 'ga' -f data/breast_cancer.csv -c 28 -s 0.5
 python3 ant/src/pkg_main/main.py -a 'ga' -f data/c2k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/Directio_site15k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/UCI_household_power_consumption50k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/Directio_site6k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/UCI_household_power_consumption19k.csv -c 28 -s 0.5
 
 python3 ant/src/pkg_main/main.py -a 'pso' -f data/breast_cancer.csv -c 28 -s 0.5
 python3 ant/src/pkg_main/main.py -a 'pso' -f data/c2k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/Directio_site15k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/UCI_household_power_consumption50k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/Directio_site6k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/UCI_household_power_consumption19k.csv -c 28 -s 0.5
 
 
 
 python3 ant/src/pkg_main/main.py -a 'aco' -f data/breast_cancer.csv -c 28 -s 0.5
 python3 ant/src/pkg_main/main.py -a 'aco' -f data/c2k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/Directio_site15k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/UCI_household_power_consumption50k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/Directio_site6k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/UCI_household_power_consumption19k.csv -c 28 -s 0.5
 
 python3 ant/src/pkg_main/main.py -a 'ga' -f data/breast_cancer.csv -c 28 -s 0.5
 python3 ant/src/pkg_main/main.py -a 'ga' -f data/c2k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/Directio_site15k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/UCI_household_power_consumption50k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/Directio_site6k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/UCI_household_power_consumption19k.csv -c 28 -s 0.5
 
 python3 ant/src/pkg_main/main.py -a 'pso' -f data/breast_cancer.csv -c 28 -s 0.5
 python3 ant/src/pkg_main/main.py -a 'pso' -f data/c2k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/Directio_site15k.csv -c 28 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/UCI_household_power_consumption50k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/Directio_site6k.csv -c 28 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/UCI_household_power_consumption19k.csv -c 28 -s 0.5
---
+++ results/swarm/README.md
@@ -0,0 +1,3 @@
+## Swarm Intelligence for GP extraction
+
+All the experiments were conducted on a [HPC Meso@LR](https://meso-lr.umontpellier.fr) platform. We used one node comprising 28 cores of CPU and 128GB of RAM.
