04a563fe187bba85bfe56d6ed8c0c450b5923ed1
owuordickson <owuordickson@gmail.com>
2021-05-10 21:19:00 +0300

    Updated

--- hpc@lr/swarmgp.sh
+++ hpc@lr/swarmgp.sh
@@ -9,52 +9,52 @@
 #SBATCH --mail-user=dickson-odhiambo.owuor@lirmm.fr
 
 module load python/3.8.2
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/Directio_site6k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/UCI_household_power_consumption19k.csv -c 14 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/breast_cancer.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/c2k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/Directio_site6k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/UCI_household_power_consumption19k.csv -c 14
 
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/Directio_site6k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/UCI_household_power_consumption19k.csv -c 14 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/breast_cancer.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/c2k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/Directio_site6k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/UCI_household_power_consumption19k.csv -c 14
 
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/Directio_site6k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/UCI_household_power_consumption19k.csv -c 14 -s 0.5
-
-
-
-
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/Directio_site6k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/UCI_household_power_consumption19k.csv -c 14 -s 0.5
-
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/Directio_site6k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/UCI_household_power_consumption19k.csv -c 14 -s 0.5
-
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/Directio_site6k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/UCI_household_power_consumption19k.csv -c 14 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/breast_cancer.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/c2k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/Directio_site6k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/UCI_household_power_consumption19k.csv -c 14
 
 
 
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/Directio_site6k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'aco' -f data/UCI_household_power_consumption19k.csv -c 14 -s 0.5
 
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/Directio_site6k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'ga' -f data/UCI_household_power_consumption19k.csv -c 14 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/breast_cancer.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/c2k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/Directio_site6k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/UCI_household_power_consumption19k.csv -c 14
 
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/Directio_site6k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'pso' -f data/UCI_household_power_consumption19k.csv -c 14 -s 0.5
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/breast_cancer.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/c2k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/Directio_site6k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/UCI_household_power_consumption19k.csv -c 14
+
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/breast_cancer.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/c2k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/Directio_site6k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/UCI_household_power_consumption19k.csv -c 14
+
+
+
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/breast_cancer.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/c2k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/Directio_site6k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'aco' -f data/UCI_household_power_consumption19k.csv -c 14
+
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/breast_cancer.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/c2k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/Directio_site6k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'ga' -f data/UCI_household_power_consumption19k.csv -c 14
+
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/breast_cancer.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/c2k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/Directio_site6k.csv -c 14
+python3 ant/src/pkg_main/main.py -a 'pso' -f data/UCI_household_power_consumption19k.csv -c 14
