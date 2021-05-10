304dfb2f5c8dc9318784311f78b8f09f2d732256
owuordickson <owuordickson@gmail.com>
2021-05-10 12:55:00 +0300

    added

--- hpc@lr/classic.sh
+++ hpc@lr/classic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=gps
-#SBATCH --output=res_main.txt
+#SBATCH --output=res_mainc.txt
 #SBATCH -n 14
 #SBATCH --time=576:00:00
 #SBATCH --partition=lirmm
@@ -11,45 +11,12 @@
 module load python/3.7.2
 #python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/breast_cancer.csv -c 14 -s 0.5
 #python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/Directio_site15k.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/Directio_site15k.csv -c 14 -s 0.5
 #python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
 
 #python3 ant/src/pkg_main/mainc.py -a 'acolcm' -f data/breast_cancer.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'acolcm' -f data/c2k.csv -c 14 -s 0.5
 #python3 ant/src/pkg_main/mainc.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
-
-
-
-
-
-
-
-#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/breast_cancer.csv -c 14 -s 0.5
-#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/Directio_site15k.csv -c 14 -s 0.5
-#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
-
-#python3 ant/src/pkg_main/mainc.py -a 'acolcm' -f data/breast_cancer.csv -c 14 -s 0.5
-#python3 ant/src/pkg_main/mainc.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
-
-
-
-
-
-
-
-
-#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/breast_cancer.csv -c 14 -s 0.5
-#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/Directio_site15k.csv -c 14 -s 0.5
-#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
-
-#python3 ant/src/pkg_main/mainc.py -a 'acolcm' -f data/breast_cancer.csv -c 14 -s 0.5
-#python3 ant/src/pkg_main/mainc.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
-
-
-
-
-
 
 
 
