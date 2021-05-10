8683c980089a2e2028e53896e46a61bb4d7d97d6
owuordickson <owuordickson@gmail.com>
2021-05-10 09:11:24 +0300

    Updated

--- hpc@lr/classic.sh
+++ hpc@lr/classic.sh
@@ -9,27 +9,13 @@
 #SBATCH --mail-user=dickson-odhiambo.owuor@lirmm.fr
 
 module load python/3.7.2
-python3 ant/src/pkg_main/main.py -a 'graank' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'graank' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'graank' -f data/Directio_site15k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'graank' -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/breast_cancer.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/c2k.csv -c 14 -s 0.5
+python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/Directio_site15k.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
 
-python3 ant/src/pkg_main/main.py -a 'acolcm' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
-
-
-
-
-
-
-
-python3 ant/src/pkg_main/main.py -a 'graank' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'graank' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'graank' -f data/Directio_site15k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'graank' -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
-
-python3 ant/src/pkg_main/main.py -a 'acolcm' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'acolcm' -f data/breast_cancer.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
 
 
 
@@ -37,14 +23,13 @@ python3 ant/src/pkg_main/main.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
 
 
 
+#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/breast_cancer.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/c2k.csv -c 14 -s 0.5
+python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/Directio_site15k.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
 
-python3 ant/src/pkg_main/main.py -a 'graank' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'graank' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'graank' -f data/Directio_site15k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'graank' -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
-
-python3 ant/src/pkg_main/main.py -a 'acolcm' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/main.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'acolcm' -f data/breast_cancer.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
 
 
 
@@ -53,6 +38,21 @@ python3 ant/src/pkg_main/main.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
 
 
 
-#python3 ant/src/pkg_main/main.py -a 'acolcm' -f data/c2k.csv -c 14 -s 0.5
-#python3 ant/src/pkg_main/main.py -a 'acolcm' -f data/c2k.csv -c 14 -s 0.5
-#python3 ant/src/pkg_main/main.py -a 'acolcm' -f data/c2k.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/breast_cancer.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/c2k.csv -c 14 -s 0.5
+python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/Directio_site15k.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
+
+#python3 ant/src/pkg_main/mainc.py -a 'acolcm' -f data/breast_cancer.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
+
+
+
+
+
+
+
+
+python3 ant/src/pkg_main/mainc.py -a 'acolcm' -f data/c2k.csv -c 14 -s 0.5
+python3 ant/src/pkg_main/mainc.py -a 'acolcm' -f data/c2k.csv -c 14 -s 0.5
+python3 ant/src/pkg_main/mainc.py -a 'acolcm' -f data/c2k.csv -c 14 -s 0.5
