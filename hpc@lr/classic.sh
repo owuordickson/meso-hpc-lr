5eda7c4c968b3a90756246f85871bbe1c1e67b9f
owuordickson <owuordickson@gmail.com>
2021-05-12 19:10:39 +0300

    Updated

--- hpc@lr/classic.sh
+++ hpc@lr/classic.sh
@@ -9,9 +9,14 @@
 #SBATCH --mail-user=dickson-odhiambo.owuor@lirmm.fr
 
 module load python/3.7.2
-python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/breast_cancer.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/c2k.csv -c 14 -s 0.5
-python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/breast_cancer.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/c2k.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'graank' -f data/UCI_household_power_consumption50k.csv -c 14 -s 0.5
 
-python3 ant/src/pkg_main/mainc.py -a 'acolcm' -f data/breast_cancer.csv -c 14 -s 0.5
+#python3 ant/src/pkg_main/mainc.py -a 'acolcm' -f data/breast_cancer.csv -c 14 -s 0.5
+python3 ant/src/pkg_main/mainc.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
+
+
+
+python3 ant/src/pkg_main/mainc.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
 python3 ant/src/pkg_main/mainc.py -a 'lcm' -f data/breast_cancer.csv -c 14 -s 0.5
