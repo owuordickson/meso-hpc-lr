6cbf56c3615f087f0203116f292ad9cb0df6caba
owuordickson <owuordickson@gmail.com>
2022-04-27 21:41:18 +0300

    added

--- hpc@lr/clugp_para2.sh
+++ hpc@lr/clugp_para2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=CLU-GP
-#SBATCH --output=res_main2.txt
+#SBATCH --output=res_main.txt
 #SBATCH -n 14
 #SBATCH --time=576:00:00
 #SBATCH --partition=lirmm
@@ -10,16 +10,21 @@
 
 module load python/3.8.2
 
-python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/breast_cancer.csv -e 0.75
-python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -e 0.75
-python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/breast_cancer.csv -e 0.75
-python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -e 0.75
-python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/breast_cancer.csv -e 0.75
-python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -e 0.75
+python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/breast_cancer.csv -i 100
+python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -i 100
+python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/breast_cancer.csv -i 100
+python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -i 100
+python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/breast_cancer.csv -i 100
+python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -i 100
 
-python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/breast_cancer.csv -e 0.9
-python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -e 0.9
-python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/breast_cancer.csv -e 0.9
-python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -e 0.9
-python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/breast_cancer.csv -e 0.9
-python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -e 0.9
+python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -i 500
+python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -i 500
+python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -i 500
+
+python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -i 1000
+python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -i 1000
+python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/breast_cancer.csv -i 1000
+
+python3 spectral_gp/src/main.py -a 'graank' -c 14 -f data/breast_cancer.csv
+python3 spectral_gp/src/main.py -a 'graank' -c 14 -f data/breast_cancer.csv
+python3 spectral_gp/src/main.py -a 'graank' -c 14 -f data/breast_cancer.csv
