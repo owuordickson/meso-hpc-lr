8a8f6f476d500d8b9703d21205b41aa80ecf0dba
owuordickson <owuordickson@gmail.com>
2022-05-02 20:58:53 +0300

    added

--- hpc@lr/spectral_gp/src/config.py
+++ hpc@lr/spectral_gp/src/config.py
@@ -9,8 +9,8 @@ CPU_CORES = 1  # Depends on your computer
 # DATASET = "../../data/hcv_data.csv"
 
 # Uncomment for Main:
-# DATASET = "../data/DATASET.csv"
-DATASET = '../data/breast_cancer.csv'
+DATASET = "../data/DATASET.csv"
+# DATASET = '../data/breast_cancer.csv'
 # DATASET = '../data/air_quality.csv'
 # DATASET = '../data/air_quality1k.csv'
 # DATASET = '../data/power_consumption19k.csv'
