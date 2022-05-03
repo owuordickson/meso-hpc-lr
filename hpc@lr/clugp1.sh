513cb192634a17962b37ff779dd7e436e824f71c
owuordickson <owuordickson@gmail.com>
2022-05-03 18:56:20 +0300

    updated

--- hpc@lr/clugp1.sh
+++ hpc@lr/clugp1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=CLU-GP
-#SBATCH --output=res_main.txt
+#SBATCH --output=res_main1.txt
 #SBATCH -n 14
 #SBATCH --time=576:00:00
 #SBATCH --partition=lirmm
@@ -10,8 +10,5 @@
 
 module load python/3.8.2
 
-python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/breast_cancer.csv -e 0.9
-python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/air_quality.csv -e 0.9
-python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/directio8k.csv -e 0.9
 python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/c2k.csv -e 0.9
 python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/power_consumption19k.csv -e 0.9
