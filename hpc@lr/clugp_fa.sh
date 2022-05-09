bfe2bd50bdd57f349736aa3eca316c9bf957252d
owuordickson <owuordickson@gmail.com>
2022-05-09 19:54:44 +0300

    added

--- hpc@lr/clugp_fa.sh
+++ hpc@lr/clugp_fa.sh
@@ -13,5 +13,6 @@ module load python/3.8.2
 python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/breast_cancer.csv -e 0.9
 python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/air_quality.csv -e 0.9
 python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/c2k.csv -e 0.9
+python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/directio8k.csv -e 0.9
 python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/directio15k.csv -e 0.9
 python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/power_consumption50k.csv -e 0.9
