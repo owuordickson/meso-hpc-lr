8d5a8935235333b2d8af3f5252db24a975189ddb
owuordickson <owuordickson@gmail.com>
2022-05-11 17:44:53 +0300

    added

--- hpc@lr/clugp.sh
+++ hpc@lr/clugp.sh
@@ -15,17 +15,16 @@ python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/air_quality.csv
 python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/c2k.csv
 python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/directio8k.csv
 python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/directio15k.csv
-python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/power_consumption50k.csv
+python3 spectral_gp/src/main.py -a 'acograd' -c 14 -f data/aps_2k.csv
 
 python3 spectral_gp/src/main.py -a 'graank' -c 14 -f data/breast_cancer.csv
 python3 spectral_gp/src/main.py -a 'graank' -c 14 -f data/air_quality.csv
 python3 spectral_gp/src/main.py -a 'graank' -c 14 -f data/c2k.csv
 python3 spectral_gp/src/main.py -a 'graank' -c 14 -f data/directio8k.csv
-python3 spectral_gp/src/main.py -a 'graank' -c 14 -f data/power_consumption50k.csv
 
 python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/breast_cancer.csv -e 0.9
 python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/air_quality.csv -e 0.9
 python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/c2k.csv -e 0.9
 python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/directio8k.csv -e 0.9
 python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/directio15k.csv -e 0.9
-python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/power_consumption50k.csv -e 0.9
+python3 spectral_gp/src/main.py -a 'clugrad' -c 14 -f data/aps_2k.csv -e 0.9
