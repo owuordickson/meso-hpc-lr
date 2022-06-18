eaf70986f78ec52ccd7c53e0143d0b5f788c8ff5
owuordickson <owuordickson@gmail.com>
2022-06-18 18:17:00 +0300

    updated

--- hpc@lr/swarmgp3.sh
+++ hpc@lr/swarmgp3.sh
@@ -11,11 +11,10 @@
 module load python/3.8.2
 
 python3 swarm_gp/src/main.py -a 'lcm' -c 14 -f data/hungary_chickenpox.csv
-python3 swarm_gp/src/main.py -a 'lcm' -c 14 -f data/air_quality.csv
-python3 swarm_gp/src/main.py -a 'lcm' -c 14 -f data/aps_5k.csv
 python3 swarm_gp/src/main.py -a 'lcm' -c 14 -f data/breast_cancer.csv
-python3 swarm_gp/src/main.py -a 'lcm' -c 14 -f data/c2k.csv
-python3 swarm_gp/src/main.py -a 'lcm' -c 14 -f data/directio15k.csv
 python3 swarm_gp/src/main.py -a 'lcm' -c 14 -f data/hcv_data.csv
+python3 swarm_gp/src/main.py -a 'lcm' -c 14 -f data/air_quality.csv
 python3 swarm_gp/src/main.py -a 'lcm' -c 14 -f data/Omnidir_site10k.csv
+python3 swarm_gp/src/main.py -a 'lcm' -c 14 -f data/directio15k.csv
 python3 swarm_gp/src/main.py -a 'lcm' -c 14 -f data/power_consumption19k.csv
+python3 swarm_gp/src/main.py -a 'lcm' -c 14 -f data/c2k.csv
