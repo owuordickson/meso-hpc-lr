dc8981a937d8c8668faaacfe1b99fd5e09b26cc4
Dickson Owuor (Ph.D.) <owuordickson@gmail.com>
2023-03-03 20:20:49 +0300

    added tests

--- hpc@lr/clugp_tuning.sh
+++ hpc@lr/clugp_tuning.sh
@@ -10,22 +10,6 @@
 
 module load python/3.8.2
 
-python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/aps_2k.csv -e 0.25
-python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/aps_2k.csv -e 0.5
-python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/aps_2k.csv -e 0.75
-python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/aps_2k.csv -e 0.9
-
-
-python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/aps_2k.csv -e 0.25
-python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/aps_2k.csv -e 0.5
-python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/aps_2k.csv -e 0.75
-python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/aps_2k.csv -e 0.9
-
-
-python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/aps_2k.csv -e 0.25
-python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/aps_2k.csv -e 0.5
-python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/aps_2k.csv -e 0.75
-python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/aps_2k.csv -e 0.9
-
-
-
+python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/c2k.csv -e 0.8
+python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/c2k.csv -e 0.8
+python3 spectral_gp/src/main1.py -a 'clugrad' -c 14 -f data/c2k.csv -e 0.8
